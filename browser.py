a9b02cb056f38f15de926d64b634e127552fa1b2
Cybot123 <67462774+Cybot123@users.noreply.github.com>
2021-02-06 13:35:55 +0800

    added hello code

--- browser.py
+++ browser.py
@@ -1,7 +1,1 @@
-# Use the Request library
-import requests
-# Set the target webpage
-url = 'http://www.wikipedia.org’
-r = requests.get(url)
-# This will get the full page
-print(r.text)
+hellodadadadd
