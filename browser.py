6f259a5deb1e09f1e8828ea63f91b553bef2a8b2
Cybot123 <67462774+Cybot123@users.noreply.github.com>
2021-02-06 13:53:42 +0800

    added code for input from user for URL

--- browser.py
+++ browser.py
@@ -1,1 +1,10 @@
-hellodadadadd
+# Use the Request library
+import requests
+# Set the target webpage
+url =input("enter your URL:")
+r = requests.get(url)
+# This will get the full page
+print(r.text)
+# This will get the status code
+print("Status code:")
+print("\t *", r.status_code)
